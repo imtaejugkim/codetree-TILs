2bcd62edee54d83925e3725d6645c7d67367124f
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-07-24 16:16:08 +0900

    Title: 달에서 무게 구하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240724/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double b = 0.165000;
+
+        System.out.print(a);
+        System.out.print(" * ");
+        System.out.printf("%.6f" ,b);
+        System.out.print(" = ");
+        System.out.printf("%.6f", a*b);
+    }
+}
