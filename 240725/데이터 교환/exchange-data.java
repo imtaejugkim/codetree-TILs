bdd4359e3f95e8aab2e2269df405f4a7dd74a625
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-07-25 13:29:08 +0900

    Title: 데이터 교환, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240725/데이터 교환/exchange-data.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int A = 0;
+        int b = 6;
+        int B = 0;
+        int c = 7;
+        int C = 0;
+
+        B = a;
+        C = b;
+        A = c;
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    }
+}
