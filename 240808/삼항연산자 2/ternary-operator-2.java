af78955ecb13f547391ed5cd4ad15f823e61faf1
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-08-08 23:39:18 +0900

    Title: 삼항연산자 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240808/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        if(a==1){
+            System.out.println("t");
+        }else{
+            System.out.println("f");
+        }
+    }
+}
