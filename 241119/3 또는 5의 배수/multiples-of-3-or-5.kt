3f6b5165066b7aa2191b3cb7d881467195a0e155
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-11-19 20:43:21 +0900

    Title: 3 또는 5의 배수, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241119/3 또는 5의 배수/multiples-of-3-or-5.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val a = readLine()!!.toInt()
+
+    if(a%3==0){
+        println("YES")
+    } else {
+        println("NO")
+    }
+    if(a%5==0){
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
