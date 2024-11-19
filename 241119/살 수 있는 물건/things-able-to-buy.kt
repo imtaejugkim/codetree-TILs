2e0c3da363900c9777d1297fe88fd1f043886c7b
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-11-19 20:24:43 +0900

    Title: 살 수 있는 물건, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 241119/살 수 있는 물건/things-able-to-buy.kt
+++ 241119/살 수 있는 물건/things-able-to-buy.kt
@@ -1,10 +1,10 @@
 fun main(){
     val moneyString = readLine()
     val money = moneyString!!.toInt()
-    if(money>3000){
+    if(money>=3000){
         print("book")
-    }else if(money>1000){
-        print("book")
+    }else if(money>=1000){
+        print("mask")
     }else {
         print("no")
     }
