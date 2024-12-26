e74b1db6d7d470af3c0aa23e5a31ab1f92b2ce6f
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-12-26 16:32:36 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 142ms, Memory: 12MB, Status: Passed - CodeTree

--- 241226/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.kt
+++ 241226/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.kt
@@ -1,8 +1,14 @@
 fun main(){
-    val a = readLine()!!.toInt()
-    val numbers = readLine()!!.split(" ").map{it.toInt()}
-
-    val envens = numbers.filter{it%2==0}
+    val n = readLine()?.toIntOrNull() ?: return println("잘못된 입력입니다.")
+    
+    // nums 값을 읽어올 때 null 체크 및 빈 리스트 체크
+    val nums = readLine()?.split(" ")?.mapNotNull { it.toIntOrNull() } ?: return println("잘못된 입력입니다.")
+    
+    // 숫자 개수와 실제 입력된 숫자 개수가 맞지 않으면 오류 처리
+    if (nums.size != n) {
+        return println("입력된 숫자의 개수와 n의 값이 맞지 않습니다.")
+    }
+    val envens = nums.filter{it%2==0}
 
     println(envens.reversed().joinToString(" "))
 }
