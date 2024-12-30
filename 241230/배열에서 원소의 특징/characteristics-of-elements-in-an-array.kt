87d426e05ae0e2afc0f9e16e808aae1f8a33df2f
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-12-30 17:52:19 +0900

    Title: 배열에서 원소의 특징, Time: 144ms, Memory: 15MB, Status: Passed - CodeTree

--- 241230/배열에서 원소의 특징/characteristics-of-elements-in-an-array.kt
+++ 241230/배열에서 원소의 특징/characteristics-of-elements-in-an-array.kt
@@ -1,6 +1,6 @@
 fun main(){
     val input = readLine()!!.trim()
-    val nums = input.split("").map { it.toInt() }
+    val nums = input.split(" ").map { it.toInt() }
 
     for(i in 1 until nums.size){
         if(nums[i]%3==0){
