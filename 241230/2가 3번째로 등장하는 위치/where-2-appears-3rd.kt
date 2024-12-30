bcf5fcc7b3524039435879bee9f03d816a768b1f
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-12-30 18:52:52 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 140ms, Memory: 12MB, Status: Runtime Error - CodeTree

---
+++ 241230/2가 3번째로 등장하는 위치/where-2-appears-3rd.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val a = readLine()!!.toInt()
+    var count = 0
+    val input = readLine()!!.split(" ").map{it.toInt()}
+
+    for(i in 0 until input.size){
+        if(input[i]==2){
+            count+=1
+            if(count==3){
+                println(i+1)
+                break
+            }
+        }
+    }
+}
